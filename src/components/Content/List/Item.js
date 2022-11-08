4671ce474318d9a1fcec2babf88dc4cc5f5c7b36
ibrahimzenk <ibrahimzenk@hotmail.com>
2022-11-09 01:41:49 +0300

    toggleTodo added

--- src/components/Content/List/Item.js
+++ src/components/Content/List/Item.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { useTodo } from "../../../contexts/TodoContext";
 
 function Item({ todo }) {
+  const { toggleTodo } = useTodo();
+
   return (
     <li className={todo.completed ? "completed" : ""}>
       <div className="view">
-        <input className="toggle" type="checkbox" checked={todo.completed} />
+        <input className="toggle" type="checkbox" checked={todo.completed} onChange={() => toggleTodo(todo.id)} />
         <label>{todo.text}</label>
         <button className="destroy"></button>
       </div>
--- src/contexts/TodoContext.js
+++ src/contexts/TodoContext.js
@@ -14,7 +14,17 @@ export const TodoProvider = ({ children }) => {
 
   const addTodo = (text) => setTodos((prev) => [...prev, { id: uuidv4(), text, completed: false }]);
 
-  const values = { todos, setTodos, addTodo };
+  const toggleTodo = (id) => {
+    const clonedTodos = [...todos];
+
+    const itemIndex = clonedTodos.findIndex((todo) => todo.id === id);
+    const item = todos[itemIndex];
+    item.completed = !item.completed;
+
+    setTodos(clonedTodos);
+  };
+
+  const values = { todos, setTodos, addTodo, toggleTodo };
 
   return <TodoContext.Provider value={values}>{children} </TodoContext.Provider>;
 };
